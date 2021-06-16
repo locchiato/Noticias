7330adeb09f9d60ccdfefcd4050748191c264e69
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-06-16 15:48:09 -0300

    [MOD] Se logra mostrar las noticias.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 window.addEventListener("load", () => {
     noticias.forEach((noticia) => {
-        if (noticia.tipoNacional) {
-            let template = `
-            <div class="noticia">
-            <img src="${noticia.imgUrl}">
-            <h2>${noticia.titulo}</h2>
-            <h6>${noticia.fecha}</h6>
-            <p>
-            ${noticia.descripcion}
-            </p>
-            </div>
-            `;
+        let template = `
+        <div class="noticia">
+        <img src="${noticia.imgUrl}">
+        <h2>${noticia.titulo}</h2>
+        <h6>${noticia.fecha}</h6>
+        <p>
+        ${noticia.descripcion}
+        </p>
+        </div>
+        `;
 
-            document.$(".nacionales").innerHTML += template;
+        let sector;
+        if (noticia.tipoNacional) {
+            sector = document.querySelector(".nacionales")
+        } else {
+            sector = document.querySelector(".internacs")
         }
+
+        sector.innerHTML += template;
+
     });
 });
