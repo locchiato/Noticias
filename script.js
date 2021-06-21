5ad9ca466001bae3f328bd8ddda035ebb3c017ef
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-06-21 06:37:26 -0300

    [MOD] Se agrega footer.

--- script.js
+++ script.js
@@ -23,12 +23,32 @@ window.addEventListener("load", () => {
     });
 
 
-    document.querySelector(".container").addEventListener("click", () => {
+    document.querySelector(".container").onclick = () => {
         document.querySelector(".sun-logo").classList.toggle("animate-sun");
         document.querySelector(".moon-logo").classList.toggle("animate-moon");
         document.querySelector("body").classList.toggle("dark");
-    })
+    };
+
+
+    function resalta(elEvento) {
+        var evento = elEvento || window.event;
+        switch (evento.type) {
+            case 'mouseover':
+                this.style.color = 'red';
+                this.style.backgroundColor = 'gold';
+                break;
+            case 'mouseout':
+                this.style.backgroundColor = 'blue';
+                this.style.color = 'white';
+                break;
+        }
+    }
+
+    document.querySelector("footer").onmouseover = resalta;
+    document.querySelector("footer").onmouseout = resalta;
+
 });
+
 
 /*
 
